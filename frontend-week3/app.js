df5fb16d7026e61f72991ece0a29e975e173c8a6
HazeemHussain <hhzmuk@gmail.com>
2023-08-10 09:23:01 +1200

    fixing minor bugs in the app

--- frontend-week3/app.js
+++ frontend-week3/app.js
@@ -2,13 +2,26 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
+
+// routes
+const books = require('./routes/api/book');
 
 const app = express();
 
-//Connecting Database
+// Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('YOO WASSUP'));
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
